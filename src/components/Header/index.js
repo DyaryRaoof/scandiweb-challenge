078799f24c2955d144cedd703c2412986fce02de
DyaryRaoof <dyary2020@gmail.com>
2022-04-15 23:40:31 +0300

    Add header with partly functional button click

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,11 @@ import logo from '../../images/logo.png';
 import emptyCartLogo from '../../images/empty-cart.png';
 import navDropdown from '../../images/nav-dropdown-icon.png';
 import NakedButton from '../Shared/NakedButton';
+import {
+    gql
+} from "@apollo/client";
+import { Query } from "@apollo/react-components";
+
 
 
 const NavULWrapper = styled.ul`
@@ -55,31 +60,48 @@ color: ${props => props.active ? '#5ECE7B' : '#1D1F22'};
 `;
 
 
-
+const CATEGORIES = gql`
+    query  {
+     categories{
+       name
+     }
+    }
+  `;
 
 class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeIndex: 0,
+        }
+    }
+
     render() {
         return (
             <nav>
                 <NavULWrapper margin>
                     <li>
                         <NavULWrapper>
-                            <NavLIWrapper active>
-                                <ActiveButton active>
-                                    WOMEN
-                                    <ActiveHR />
-                                </ActiveButton>
-                            </NavLIWrapper>
-                            <NavLIWrapper>
-                                <ActiveButton>
-                                    MEN
-                                </ActiveButton>
-                            </NavLIWrapper>
-                            <NavLIWrapper>
-                                <ActiveButton>
-                                    KIDS
-                                </ActiveButton>
-                            </NavLIWrapper>
+                            <Query query={CATEGORIES}>
+                                {({ loading, error, data }) => {
+                                    if (loading) return <p>Loading...</p>;
+                                    if (error) return <p>Error :(</p>;
+
+                                    return data.categories.map((category, index) => {
+                                        return (
+
+                                            <NavLIWrapper key={index}>
+                                                <ActiveButton active={this.state.activeIndex === index} onClick={() => this.setState({ activeIndex: index })}>
+                                                    {category.name.toUpperCase()}
+                                                    {this.state.activeIndex === index ? <ActiveHR /> : null}
+                                                </ActiveButton>
+                                            </NavLIWrapper>
+                                        )
+                                    })
+                                }
+                                }
+                            </Query>
                         </NavULWrapper>
                     </li>
                     <LogoLI>
